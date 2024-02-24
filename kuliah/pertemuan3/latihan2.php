18a88f4991cbf54509c073167deb71170af2def5
Amekiri1 <amekiri94@gmail.com>
2024-02-24 15:51:19 +0700

    first commit

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,6 @@
+<?php
+$nama = "Deri";
+$umur = 19;
+$kampus = "Universitas Pasundan";
+echo "Halo, nama saya $nama. Umur saya $umur tahun, saya kuliah di $kampus.";
+?>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,12 @@
+<?php
+$npm_deri = 22; // Ganti dengan dua digit terakhir NPM Anda
+echo "Aku adalah angka <strong> $npm_deri </strong> <br>";
+$npm_deri *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <strong> $npm_deri </strong> <br>";
+$npm_deri /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <strong> $npm_deri </strong> <br>";
+$npm_deri += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <strong> $npm_deri </strong> <br>";
+$npm_deri -= 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <strong> $npm_deri </strong> <br>";
+?>
